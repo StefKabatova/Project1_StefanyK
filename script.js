1a84c1e9c54fb4e2c9883d0482170515eef9bd4f
StefKabatova <stefkabatova@gmail.com>
2016-10-16 12:35:36 -0400

    added function that checks for ids to provide specific content

--- script.js
+++ script.js
@@ -8,16 +8,19 @@ $(document).ready(function(){
   var question = $("#question")
   var answers = $(".answers")
   var next = $("#next");
-  var container= $("#gameContainer")
+  var container= $("#gameContainer");
+  var categories = $(".quesType");
+
 
   var roundNumb = 1;
   var counter = 0;
   var msg;
   var option;
   var score = 0;
+  var category;
 
-  // set of first questions
-  var gameQuestRound1 ={
+  // set of content for entertainment category
+  var entertainment ={
         question:[" What are the two main flavors in Nutella? ",
         " What does wi-fi stand for?", "two"],
         answer:[["Chocolate & Hazelnut",
@@ -42,35 +45,94 @@ $(document).ready(function(){
        }
      }
 
+     var geography = {
+     question:[" Is Donald Duck an only child? ", "perro", "gato"],
+     answer:[["Chocolate","Cocoa","Dark  & Vanilla"],
+     [ "Wireless-filter ", "Wire-film", "Wireless-fidelity"]],
+     content: function(i){
+       console.log(this)
+       return this.question[i]
+     },
+     options: function(o){
+       return this.answer[o]
+     },
+     correct: function (){
+       score += 100
+       $("#score").html("Score" + " " + score)
+       return console.log("right");
+     },
+     incorrect: function(){
+       score += 0
+       return console.log("wrong");
+     },
+    }
+
+    var stem = {
+    question:[" Is Donald Duck an only child? ", "perro", "gato"],
+    answer:[["Chocolate","Cocoa","Dark  & Vanilla"],
+    [ "Wireless-filter ", "Wire-film", "Wireless-fidelity"]],
+    content: function(i){
+      console.log(this)
+      return this.question[i]
+    },
+    options: function(o){
+      return this.answer[o]
+    },
+    correct: function (){
+      score += 100
+      $("#score").html("Score" + " " + score)
+      return console.log("right");
+    },
+    incorrect: function(){
+      score += 0
+      return console.log("wrong");
+    },
+   }
 
      // sets questions and answers on screen
-      function arrayValue(){
-        round.text("Round: " + roundNumb)
-      var msg =  gameQuestRound1.content(counter);
-      // console.log(msg)
-      question.text(msg);
 
-      for (i=0;i<=3; i++){
-        var option = gameQuestRound1.options(counter)[i];
-          answers.each(function(){
-          $(answers[i]).html(option)
-            })
-          }
-        };
+     function addmsg(){
 
+       if($(this).is("#enews")){
+        console.log($(this))
+        category = entertainment
+        msg = category.content(counter);
+       }
+
+       else if($(this).is("#geo")){
+       console.log($(this))
+       category= geography
+       msg = category.content(counter);
+       }
+       else{
+         category= stem
+         msg = category.content(counter);
+       }
+       question.text(msg);
+
+       for (i=0;i<=3; i++){
+         var option = category.options(counter)[i];
+           answers.each(function(){
+           $(answers[i]).html(option)
+             })
+           }
+
+     }
+
+
+      // checks correctness of the answers
 
         function validity(){
            if($(this).is("#correct")){
-            gameQuestRound1.correct();
+            category.correct();
           }else{
-            gameQuestRound1.incorrect();
+            category.incorrect();
           }
         }
 
+        categories.on("click", addmsg)
 
-        beginButton.on("click", function(){
-            container.css("display", 'block')
-            arrayValue()});
+
 
         answers.on("click", validity);
         next.on("click" ,function(){
@@ -79,4 +141,27 @@ $(document).ready(function(){
             arrayValue();
           });
 
+
+
+
 });
+
+
+
+// function arrayValue(){
+//   round.text("Round: " + roundNumb)
+//  msg =  gameQuestRound1.content(counter);
+// // console.log(msg)
+// question.text(msg);
+//
+// for (i=0;i<=3; i++){
+//   var option = gameQuestRound1.options(counter)[i];
+//     answers.each(function(){
+//     $(answers[i]).html(option)
+//       })
+//     }
+//   };
+
+// beginButton.on("click", function(){
+//     container.css("display", 'block')
+//     arrayValue()});
