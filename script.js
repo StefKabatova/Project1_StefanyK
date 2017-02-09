ae9566abb5a37ea43fb8ed3d1dcd097d84013b21
StefKabatova <stefkabatova@gmail.com>
2017-02-09 12:44:35 -0500

    Updated JS

--- script.js
+++ script.js
@@ -1,232 +1,169 @@
 $(document).ready(function(){
-  console.log("hello")
+
+  var first_catg = $("#enews");
+  var second_catg = $("#geo");
+  var third_catg = $("#stem");
+  var question = $("#question");
+  var answer= $("#answer");
+  var options = $(".options");
+  var category_type = $("#category");
+  var category_options = $("#category_box")
+  var gamebox = $("#gamebox")
+  var next = $("#next_button")
 
 
-  // html element references
-  var beginButton = $("#start");
-  var round = $('#round')
-  var question = $("#question")
-  var answers = $(".answers")
-  var next = $("#next");
-  var container= $("#gameContainer");
-  var categories = $(".quesType");
-  var popUp = $("#answerMsg");
-  var cont_button = $("#continueE");
-
-
-  //James's Note: make stem, geography, entertainment
-  // var gameObjects = [stem, geography, entertainment] ???
-  var round = 0;
-  var counter = 0;
-  var msg;
-  var option;
-  var score = 0;
+  var data;
   var category;
-  var continueButton;
+  var points = 0;
+  var round = 0;
+  var index = 0;
+  var correct_answer ;
+  var answer_selection;
 
+  let entertainment_data = [{
+    question: "Which film had a worldwide box-office gross income of over $2.7billion? ",
+    choices: ["Titanic","Lion King","Jurassic Park","Avatar"],
+    answer: "Avatar"
+  },{
+    question:"Which actress has been nominated for an Academy Award more than 19 times, and has won the award more than 3 times?",
+    choices:["Julia Roberts","Kristen Stewart","Emily Blunt","Meryl Streep"],
+    answer: "Meryl Streep"},
+    {
+      question:"How old was Daniel Radcliffe when he played in the first Harry Potter Movie?",
+      choices:["12","11","13","15"],
+      answer: "11"}
+  ]
 
-  // set of content for entertainment category
-  var entertainment ={
-        question:["The sounds made by the Brachiosaurs in Jurassic Park were a combination of ",
-         " What type of candy does Elliott use to persuade E.T to come into his room? ",
-         "What is the name of Jon Snow’s Direwolf ?"],
-        answer:[
-          ["Truck and a horse", "Giraffe and a hippo", "<p id='correct'>Whales and Donkies</p>"],
-          [ "Skittles ", "M&M'S", "<p id='correct'>Reese's Pieces</p>"],
-          ["Grey Wind", "Graham", "<p id='correct'>Ghost</p>"]
-        ],
-       correct: function (){
-         score += 100
-         $("#score").html("Score" + " " + score)
-
-         return console.log("right");
-       },
-       incorrect: function(){
-         score += 0
-
-         return console.log("wrong");
-       },
-       content: function(i){
-
-         return this.question[i]
-
-       },
-       options:function(o){
-         return this.answer[o]
-       }
-     }
-
-     var geography = {
-     question:[ "What language do the locals speak in Bogota, Colombia?",
-     "What is the Capital of Qatar",
-     "What is the lasrgest country in the World"],
-
-     answer:[["French","<p id='correct'>Spanish</p>","Arabic"],
-     ["Stockholm", "Cairo","<p id='correct'>Doha</p>"],
-     ["China", "India", "<p id='correct'>Russia</p>"]],
-
-     content: function(i){
-       console.log(this)
-       return this.question[i]
-     },
-     options: function(o){
-       return this.answer[o]
-     },
-     correct: function (){
-       score += 100
-       $("#score").html("Score" + " " + score)
-
-       return console.log("right");
-     },
-     incorrect: function(){
-       score += 0
-
-       return console.log("wrong");
-     },
+  let geo_data = [{
+    question: "What is the largest island in the Mediterranean?",
+    choices: ["Sicily","Corsia", "Crete", "Cyprus"],
+    answer: "Sicily"
+  },{
+    question:"What is the largest desert in the world?",
+    choices:["Kalahari Desert","Mojave Desert","Sahara Desert","Sinai Desert"],
+    answer: "Sahara Desert"},
+    {
+      question: "Where is the Bora Bora island located?",
+      choices: ["Bahamas", "French Polynesia", "Grenada", "Colombia"],
+      answer: "French Polynesia"
     }
+  ]
 
-    var stem = {
-    question:[" What does wi-fi stand for?", "The Earth is how many miles away from the sun?", "A shrimp's heart is located in which part of its body?"],
-    answer:[[ "Wireless-filter ", "Wire-film", "<p id='correct'>Wireless-fidelity</p>"],
-    ["<p id='correct'>92.96 miles</p>", "86.54 mil miles", "82.46 mil. miles"],
-    ["<p id='correct'>head</p>", "stomach","tail"]],
-    content: function(i){
-      console.log(this)
-      return this.question[i]
-    },
-    options: function(o){
-      return this.answer[o]
-    },
-    correct: function (){
-      score += 100
-      $("#score").html("Score:" + " " + score)
-
-      return console.log("right");
-    },
-    incorrect: function(){
-      score += 0
-
-      return console.log("wrong");
-    },
-   }
-
-    // Initiates game and shows categories
-    beginButton.on("click", function(){
-       categories.show(1000)
-
-     })
-
-     // sets questions and answers on screen based on category
-
-     function addmsg(category){
-       round += 1
-       category = category
-       msg = category.content(counter)
-
-       question.text(msg);
-
-       for (i=0;i<3; i++){
-         var option = category.options(counter)[i];
-           answers.each(function(){
-           $(answers[i]).html(option)
-            })
+  let stem_data = [{
+    question: "What does wi-fi stand for?",
+    choices: ["web interface filter","wireless fiber-optic","wireless fidelity","wireless local network"],
+    answer: "wireless fidelity"
+  },{
+    question:"How far is it from Earth to the moon?",
+    choices:["245,876mi","234,900mi","283,800mi","238,900mi"],
+    answer: "238,900mi"},
+    {
+      question:"What's the name of the first supercomputer",
+      choices:["Atlas","Cray","Watson","Jennings"],
+      answer: "Cray"}
+  ]
 
 
+// set an event listeners on the categories
+  first_catg.on("click", function(){
+    category = entertainment_data
+// shows which category was selected
+    category_type.text("Category: Entertainment")
+// passes selected category data as a parameter to the function add_content
+    status_game()
+  })
+  second_catg.on("click", function(){
+      category_type.text("Category: Geography")
+      category = geo_data;
+      status_game()
+  })
+  third_catg.on("click", function(){
+      category_type.text("Category: Science & Technology")
+      category = stem_data;
+      status_game()
+  })
 
-          };
+
+  function status_game(){
+      gamebox.show(1000)
+      category_options.hide("slow")
+    if (round <3 && index<3) {
+      category = category
+      add_content(category)
+    }else{
+      round= 0
+      index= 0
+      category_type.empty()
+      question.empty()
+      options.empty()
+      $("#point").empty()
+      gamebox.delay(500).hide("slow")
+      next.text("Next")
+      category_options.show("slow")
+      points = 0; 
+    }
+  }
+
+
+// function retrieves data for: question, answer choices, and answer.
+  function add_content(category){
+    data = category[index]
+    for (key in data){
+      if (key == "question"){
+        $("#question").text(data[key])
         }
+      if (key == "choices"){
+    // goes through the choices array and provides a value and index
+        data[key].forEach(function(item,index){
+    // saves array content on based on the index
+        var thing = item
+    // inputs content on its respective box
+       options.eq(index).text(thing)
+        })
+      }
+      if (key == "answer"){
+  // stores answer on a variable, so it can be checked
+      correct_answer = data[key]
+      }
+    }
+  }
+
+  function game_checker(answer){
+    if (answer == correct_answer){
+      $("#question").text("Correct!")
+      points += 100
+    }
+    else {
+      $("#question").text("INCORRECT! "+ "The Correct answer is :"  + " " + correct_answer)
+      // points -=50
+    }
+    $("#point").html("Total Points:" + "\n" + parseInt(points))
+
+    next.show()
+   };
+
+   // sets an event listener on the answer choices and retrieves chosen value
+   options.on("click", function(){
+     answer_selection = $(this).text();
+     // calls game_checker function to test if correct
+     game_checker(answer_selection)
+     round +=1
+     index +=1
+
+   })
 
 
-      // checks response for correctness
-        function validity(){
-          cont_button.text("continue");
+   next.on("click", function(){
+     if (round == 2){
+       next.text("Play Again?")
 
-           if($(this).children().first().is("#correct")){
+     }
+          next.hide()
+          status_game()
 
-             if(counter>=2){
-
-                cont_button.text("Try another category");
-                cont_button.css("font-weight", "bold")
-
-             }
-             category.correct();
-              $("#right").text("You Are Correct!!")
-              popUp.show()
-
-          }else{
-
-            if(counter>=2){
-              cont_button.text("Try another category");
-              cont_button.css("font-weight", "bold");
-
-            }
-            category.incorrect();
-            $("#right").text("WRONG!!!")
-            popUp.show()
-          }
-
-        }
-
-
-
-
-        // identifies which category was selected
-          categories.on("click", function(){
-
-            if (round >3){
-             round = 0;
-             counter = 0  ;
-             console.log(round)
-           }
-
-
-            if($(this).is("#enews")){
-            console.log($(this))
-            category = entertainment;
-            addmsg(category);
-              }
-            else if($(this).is("#geo")){
-            console.log($(this))
-            category= geography;
-            addmsg(category);
-              }
-            else{
-            category= stem
-            addmsg(category);
-                 }
-            });
-
-        // continue button to display following button.
-        cont_button.on("click" ,function(){
-          if(round<3){
-            counter += 1;
-          }
-          popUp.hide(800)
-          console.log(category);
-          addmsg(category);
-
-        });
+       })
+  next.hide()
 
 
 
-        // begins with a hidden continue button
-        popUp.hide()
-
-        // hides categories to be displayed after start button
-        categories.hide()
-
-        // changes opacity based on users engagement with the element
-        answers.hover(function(){
-          $(this).fadeTo('fast',1)},
-          function(){
-          $(this).fadeTo('fast',0.4)
-        });
-
-        answers.on("click", validity);
-
-
-
-
-
-
-});
+})
